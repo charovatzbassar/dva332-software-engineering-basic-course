e50916a9cf8b1d46db697104e422de1311e1cbc5
Basar Carovac <carovac.basar@gmail.com>
2024-10-19 14:42:33 +0200

    init

---
+++ src/main/java/Account.java
@@ -0,0 +1,143 @@
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class Account implements IAccount {
+
+    /**
+     * Current balance this account holds
+     */
+    private BigDecimal balance;
+    /**
+     * Currency used in this account, can be "SEK", "EUR", or "USD"
+     */
+    private String currency;
+    /**
+     * max_overdrawn is a non-negative number indicating how much the account can be "in the red"
+     * The minimum balance of the account is -1 * max_overdrawn
+     */
+    private BigDecimal max_overdrawn;
+
+    // The list of possible currencies
+    private final List<String> currencies = Arrays.asList("SEK", "EUR", "USD");
+
+    public BigDecimal getMaxOverdrawn() {
+        return this.max_overdrawn;
+    }
+
+    public void setMaxOverdrawn(BigDecimal max_overdrawn) {
+        if(max_overdrawn.compareTo(BigDecimal.ZERO) <= 0) {
+            this.max_overdrawn = BigDecimal.ZERO;
+        } else {
+            this.max_overdrawn = max_overdrawn;
+        }
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        /*
+        It needs to be checked if the user has chosen USD, EUR or SEK. If not, do not take further action.
+         */
+        if (!this.currencies.contains(currency)) return;
+        this.currency = currency;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        /*
+        Here, the bug was that you could not set the balance to be zero. Additionally, I have
+        modified the expression using the negation rule in tautology to remove the extra negation
+        */
+        if (balance.compareTo(this.max_overdrawn.multiply(new BigDecimal(-1))) >= 0) {
+            this.balance = balance;
+        }
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public Account() {
+        this.balance = BigDecimal.ZERO;
+        this.currency = "SEK";
+        this.max_overdrawn = BigDecimal.ZERO;
+    }
+
+    public Account(BigDecimal starting_balance, String currency, BigDecimal max_overdrawn) {
+        // checks if the balance is greater than -1*max_overdrawn value
+        this.balance = starting_balance.compareTo(max_overdrawn.multiply(new BigDecimal(-1))) < 0 ? BigDecimal.ZERO : starting_balance;
+        // checks if the currency is valid
+        this.currency = this.currencies.contains(currency) ? currency : "SEK";
+        // Rewritten it using the ternary operator
+        this.max_overdrawn = max_overdrawn.compareTo(BigDecimal.ZERO) <= 0 ? BigDecimal.ZERO : max_overdrawn;
+    }
+
+    @Override
+    public BigDecimal withdraw(BigDecimal requestedAmount) {
+        /*
+        The first implementation just removed the funds without any checks.
+        Firstly, it has to be checked whether the projected balance exceeds the max_overdrawn value, and if the requestedAmount is positive.
+        Then, the subtraction did not reflect on the class attribute balance.
+        If the money can be withdrawn, the class attribute is updated, and the new balance is returned.
+        */
+        if (requestedAmount.compareTo(BigDecimal.ZERO) < 0) return this.balance;
+
+        BigDecimal projBalance = this.balance.subtract(requestedAmount);
+
+        if (projBalance.compareTo(this.max_overdrawn.multiply(new BigDecimal(-1))) < 0) return this.balance;
+
+        BigDecimal newBalance = this.balance.subtract(requestedAmount);
+        this.balance = newBalance;
+        return newBalance;
+    }
+
+    @Override
+    public BigDecimal deposit(BigDecimal amount_to_deposit) {
+        /*
+          Here, it firstly needs to be checked whether amount_to_deposit is negative.
+          Then, the new balance will be set as the value of the balance attribute, and the new balance will be returned.
+         */
+        if (amount_to_deposit.compareTo(BigDecimal.ZERO) < 0) return this.balance;
+
+        BigDecimal newBalance = this.balance.add(amount_to_deposit);
+        this.balance = newBalance;
+        return newBalance;
+    }
+
+    @Override
+    public void convertToCurrency(String currencyCode, double rate) {
+        /*
+        Before the conversion, the currency code must be valid, and the rate must be greater than 0.
+        Also, the balance attribute was not updated, so that was also implemented.
+         */
+        if (rate <= 0 || !this.currencies.contains(currencyCode)) return;
+
+        this.currency = currencyCode;
+        this.balance = this.balance.multiply(new BigDecimal(rate));
+    }
+
+    @Override
+    public void TransferToAccount(Account to_account) {
+        /*
+        Because the IAccount interface did not define getters and setters for the fields, I have changed the signature
+        of this method, so that it accepts the implemented class, not the class which implements that interface.
+        In order for the transfer to happen, the currencies must be the same. Here, if they are different,
+        no further action will be taken. Also, the account on which this method is called on must be updated with the new balance, i.e. 0.
+         */
+        if (!this.currency.equals(to_account.getCurrency())) return;
+
+        to_account.deposit(this.balance);
+        this.setBalance(BigDecimal.ZERO);
+    }
+
+    @Override
+    public BigDecimal withdrawAll() {
+        /*
+        In order to withdraw all positive funds, it needs to be checked if there are any positive funds, i.e. if the balance is positive.
+        The current balance will be returned if the balance is negative.
+         */
+        return this.balance.compareTo(BigDecimal.ZERO) > 0 ? this.withdraw(this.balance) : this.balance;
+    }
+}
---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,185 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+class AccountTest {
+
+    @Test
+    void testGetMaxOverdrawn() {
+        Account myTestAccount = new Account(BigDecimal.ZERO, "SEK", BigDecimal.ZERO);
+        assertEquals(BigDecimal.ZERO, myTestAccount.getMaxOverdrawn());
+
+        Account myTestAccount2 = new Account(BigDecimal.ZERO, "SEK", new BigDecimal(-1));
+        assertEquals(BigDecimal.ZERO, myTestAccount2.getMaxOverdrawn()); //max_overdrawn must be non-negative
+
+        Account myTestAccount3 = new Account(BigDecimal.ZERO, "SEK", new BigDecimal(1000));
+        assertEquals(new BigDecimal(1000), myTestAccount3.getMaxOverdrawn());
+    }
+
+    @Test
+    void testSetMaxOverdrawn() {
+        Account myTestAccount = new Account(BigDecimal.ZERO, "SEK", BigDecimal.ZERO);
+
+        myTestAccount.setMaxOverdrawn(new BigDecimal(-1));
+        assertEquals(BigDecimal.ZERO, myTestAccount.getMaxOverdrawn()); //max_overdrawn must be non-negative
+
+        myTestAccount.setMaxOverdrawn(new BigDecimal(100));
+        assertEquals(new BigDecimal(100), myTestAccount.getMaxOverdrawn()); //max_overdrawn must be non-negative
+
+        myTestAccount.setMaxOverdrawn(new BigDecimal(1234567890));
+        assertEquals(new BigDecimal(1234567890), myTestAccount.getMaxOverdrawn()); //max_overdrawn must be non-negative
+
+    }
+
+    @Test
+    void testGetCurrency() {
+        Account myTestAccount = new Account(BigDecimal.ZERO,  "SEK", BigDecimal.ZERO);
+        assertEquals("SEK", myTestAccount.getCurrency());
+
+        myTestAccount = new Account(BigDecimal.ZERO,  "EUR", BigDecimal.ZERO);
+        assertEquals("EUR", myTestAccount.getCurrency());
+
+        myTestAccount = new Account(BigDecimal.ZERO,  "USD", BigDecimal.ZERO);
+        assertEquals("USD", myTestAccount.getCurrency());
+    }
+
+    @Test
+    void testSetCurrency() {
+        Account myTestAccount = new Account(BigDecimal.ZERO, "SEK", BigDecimal.ZERO);
+        myTestAccount.setCurrency("EUR");
+        assertEquals("EUR", myTestAccount.getCurrency());
+
+        myTestAccount.setCurrency("SEK");
+        assertEquals("SEK", myTestAccount.getCurrency());
+
+        myTestAccount.setCurrency("USD");
+        assertEquals("USD", myTestAccount.getCurrency());
+
+        myTestAccount.setCurrency("BAM");
+        assertEquals("USD", myTestAccount.getCurrency());
+    }
+
+    @Test
+    void testGetBalance() {
+        Account myTestAccount = new Account(BigDecimal.ZERO, "SEK", BigDecimal.ZERO);
+        assertEquals(BigDecimal.ZERO, myTestAccount.getBalance());
+
+        myTestAccount = new Account(new BigDecimal(100), "SEK", BigDecimal.ZERO);
+        assertEquals(new BigDecimal(100), myTestAccount.getBalance());
+
+        myTestAccount = new Account(new BigDecimal(1234567890), "SEK", BigDecimal.ZERO);
+        assertEquals(new BigDecimal(1234567890), myTestAccount.getBalance());
+    }
+
+    @Test
+    void testSetBalance() {
+        Account myTestAccount = new Account(BigDecimal.ZERO, "SEK", BigDecimal.ONE);
+
+        //should not be allowed to set balance to lower that -1 * maxOverdrawn
+        myTestAccount.setBalance(new BigDecimal(-2));
+        assertEquals(BigDecimal.ZERO, myTestAccount.getBalance());
+
+        myTestAccount.setBalance(new BigDecimal(42));
+        assertEquals(new BigDecimal(42), myTestAccount.getBalance());
+
+        myTestAccount.setBalance(new BigDecimal(1234567890));
+        assertEquals(new BigDecimal(1234567890), myTestAccount.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        Account testAccount = new Account(new BigDecimal(1000), "SEK", BigDecimal.ZERO);
+
+        testAccount.withdraw(new BigDecimal(500));
+        assertEquals(new BigDecimal(500), testAccount.getBalance());
+
+        testAccount.withdraw(new BigDecimal(501));
+        assertEquals(new BigDecimal(500), testAccount.getBalance());
+
+        testAccount.withdraw(new BigDecimal(-5));
+        assertEquals(new BigDecimal(500), testAccount.getBalance());
+
+        Account testAccount2 = new Account(new BigDecimal(-1), "SEK", new BigDecimal(Double.MIN_VALUE));
+        testAccount2.withdraw(new BigDecimal(Double.MAX_VALUE));
+        assertEquals(BigDecimal.ZERO, testAccount2.getBalance());
+
+    }
+
+    @Test
+    void testDeposit() {
+        Account testAccount = new Account(new BigDecimal(1000), "SEK", BigDecimal.ZERO);
+
+        testAccount.deposit(new BigDecimal(1000));
+        assertEquals(new BigDecimal(2000), testAccount.getBalance());
+
+        testAccount.deposit(new BigDecimal(1234567890));
+        assertEquals(new BigDecimal(1234569890), testAccount.getBalance());
+
+        testAccount.deposit(new BigDecimal(-12));
+        assertEquals(new BigDecimal(1234569890), testAccount.getBalance());
+
+    }
+
+    @Test
+    void testConvertToCurrency() {
+        Account account = new Account(new BigDecimal(1200), "SEK", BigDecimal.ZERO);
+        account.convertToCurrency("BAM", 0.17);
+        assertEquals(new BigDecimal(1200), account.getBalance());
+
+        account.convertToCurrency("EUR", 0.09);
+        assertEquals(new BigInteger("107"), account.getBalance().toBigInteger());
+
+        account.convertToCurrency("BLA", -1);
+        assertEquals(new BigInteger("107"), account.getBalance().toBigInteger());
+    }
+
+    @Test
+    void testTransferToAccount() {
+        Account account1 = new Account(new BigDecimal(1200), "SEK", BigDecimal.ZERO);
+        Account account2 = new Account(new BigDecimal(1200), "SEK", BigDecimal.ZERO);
+
+        account1.TransferToAccount(account2);
+
+        assertEquals(BigDecimal.ZERO, account1.getBalance());
+        assertEquals(new BigDecimal(2400), account2.getBalance());
+
+        Account account3 = new Account(new BigDecimal(1200), "SEK", BigDecimal.ZERO);
+        Account account4 = new Account(new BigDecimal(1200), "EUR", BigDecimal.ZERO);
+
+        account3.TransferToAccount(account4);
+
+        assertEquals(new BigDecimal(1200), account3.getBalance());
+        assertEquals(new BigDecimal(1200), account4.getBalance());
+
+        Account account5 = new Account(new BigDecimal(1200), "SEK", BigDecimal.ZERO);
+        Account account6 = new Account(new BigDecimal(12321), "SEK", BigDecimal.ZERO);
+
+        account6.TransferToAccount(account5);
+
+        assertEquals(new BigDecimal(13521), account5.getBalance());
+        assertEquals(BigDecimal.ZERO, account6.getBalance());
+    }
+
+    @Test
+    void testWithdrawAll() {
+        Account account = new Account(new BigDecimal(1200), "SEK", BigDecimal.ZERO);
+        account.withdrawAll();
+
+        assertEquals(BigDecimal.ZERO, account.getBalance());
+
+        Account account1 = new Account(new BigDecimal(1200), "EUR", new BigDecimal(250));
+        account1.withdrawAll();
+
+        assertEquals(BigDecimal.ZERO, account1.getBalance());
+
+        Account account2 = new Account(new BigDecimal(1200), "USD", new BigDecimal(1234567890));
+        account2.withdrawAll();
+
+        assertEquals(BigDecimal.ZERO, account2.getBalance());
+    }
+}
